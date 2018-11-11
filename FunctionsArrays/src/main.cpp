cf96d37e517735714959d7a1499b1a5fe8bc2255
Abdullah Alger <abdullah.alger@ibm.com>
2018-11-11 13:35:42 -0800

    added files

---
+++ FunctionsArrays/src/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+void print_array(const int arr[], size_t size);
+void set_array(int arr[], size_t size, int value);
+
+void print_array(const int arr[], size_t size) {
+    for (size_t i {0}; i < size; i++) {
+        cout << arr[i] << " ";
+        cout << endl;
+    }
+}
+
+void set_array(int arr[], size_t size, int value) {
+    for(size_t i {0}; i < size; i++) {
+        arr[i] = value;
+    }
+}
+
+int main() {
+
+    int my_scores[] {100, 98, 97, 23, 19};
+    print_array(my_scores, 5);
+    set_array(my_scores, 5, 10);
+    print_array(my_scores, 5);
+}
